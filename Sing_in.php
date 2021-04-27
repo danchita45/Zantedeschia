531897859746f88966862331df73d8e2b2ec175e
NaaraiGasca <snai2016@gmail.com>
2021-04-27 10:58:45 -0500

    Modifique el login y el sing_in

--- Sing_in.php
+++ Sing_in.php
@@ -1,23 +1,26 @@
-<?php 
-require './database.php';
-$message='';
-if (!empty($_POST['Correo']) && !empty($_POST['Contra']));
-$sql = "INSERT INTO cliente (Contra, Correo, Nombre) Values (:Contra,:Correo,:Nombre)";
-$stmt= $conn ->prepare ($sql);
-$stmt->bindParam(':Correo', $_POST['Contra']);
-$password = password_hash($_POST,['Contra'], PASSWORD_BCRYPT );
-$stmt->bindParam(':Contra',$passwordd);
+<?php
 
-if ($stmt->execute()) {
-    $message ='Se ha creado correctamente';
-    
-}  else {
-    $message = 'ha ocurrido un error, verifique los datos';
-    
-        
-}?>
+require 'database.php';
 
+$message = '';
 
+if (!empty($_POST['Contra']) && !empty($_POST['Correo']) && !empty($_POST['Nombre']))
+{
+  $sql = "INSERT INTO cliente (Contra, Correo, Nombre) VALUES (:Contra, :Correo, :Nombre)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bindParam(':Correo', $_POST['Correo']);
+  $password = password_hash($_POST['Contra'], PASSWORD_BCRYPT);
+  $stmt->bindParam(':Contra', $password);
+
+  if ($stmt -> execute() ) 
+  {
+    $message = 'Se ha creado su cuenta correctamente';
+  } else 
+  {
+    $message = 'Ha ocurrido un error, verifique los datos o intente nuevamente';
+  }
+}
+?>
 
 <!DOCTYPE html>
 <html>
@@ -32,13 +35,14 @@ if ($stmt->execute()) {
     
     <body>  
 <?php require './partials/header.php';?>
+
+    <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
         
-       <?php 
-        if (!empty($message)): ?> 
-        <p> <?php $message ?></p>
-        <?php endif;?>
         <h1> Registrarse</h1>
-        <form action="Sing_in.php" method="post">
+
+        <form action="loginphp.php" method="post">
             <input type="text" name="Correo" placeholder="ingresa tu correo electronico">
              <input type="password" name="Contra" placeholder="ingresa tu contraseña">
              <input type="text" name="Nombre" placeholder="ingresa tu sobrenombre">
--- loginphp.php
+++ loginphp.php
@@ -1,8 +1,37 @@
+<?php
+
+    session_start();
+    
+    if (isset($_SESSION['user_ID_cliente'])) {
+        header('Location: /php-login');
+    }
+
+    require 'database.php';
+
+    /*if (!empty($_POST['Correo']) && !empty($_POST['Contra'])) {
+        $records = $conn->prepcare('SELECT ID_cliente, Correo, password FROM cliente WHERE Correo = :Correo');
+        $records->bindParam(':Correo', $_POST['Correo']);
+        $records->execute();
+        $results = $records->fetch(PDO::FETCH_ASSOC);
+
+        $message = '';
+
+        if (count($results) > 0 && password_verify($_POST['Contra'], $results['Contra'])) {
+            $_SESSION['user_id'] = $results['ID_cliente'];
+            header("Location: /php-login");
+        } else 
+        {
+            $message = 'Error, los datos no coinciden';
+        }
+    }
+  }*/
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title> login</title>
+        <title> Login</title>
         
         <link rel="stylesheet" href="Assets/ccs/estilos.css">
         
@@ -16,8 +45,8 @@
         </h1>
         <span> si no tienes cuenta <a href="Sing_in.php"> Registrate </a> </span>
         <form action="loginphp.php" method="post">
-            <input type="text" name="Correo" placeholder="ingresa tu correo electronico">
-             <input type="password" name="Contra" placeholder="ingresa tu contraseña">
+            <input type="text" name="Correo" placeholder="Ingresa tu correo electronico">
+             <input type="password" name="Contra" placeholder="Ingresa tu contraseña">
              <input type="submit" value="Ingresar"  >
         </form>
     
